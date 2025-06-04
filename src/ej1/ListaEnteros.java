819376f28c39c5ed2c437efa96018cce20565d23
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-04 12:02:17 +0200

    Actualizacion ListaEnteros

--- src/ej1/ListaEnteros.java
+++ src/ej1/ListaEnteros.java
@@ -11,55 +11,53 @@ public class ListaEnteros {
 
 	public static void main(String[] args) {
 
-		//ejercicio1
-		List<Integer> listaNum = Arrays.asList(1,2,3,4,5);
-		
+		// ejercicio1
+		List<Integer> listaNum = Arrays.asList(1, 2, 3, 4, 5);
+
 		System.out.println(listaNum);
 		System.out.println();
-		
-		//ejercicio2
-		
-		HashSet<String> coche= new HashSet<>();
-		
-		String c1 = new String ("Toyota");
-		String c2 = new String ("Toyota");
-		String c3 = new String ("Seat");
-		String c4 = new String ("Seat");
-		
+
+		// ejercicio2
+
+		HashSet<String> coche = new HashSet<>();
+
+		String c1 = new String("Toyota");
+		String c2 = new String("Toyota");
+		String c3 = new String("Seat");
+		String c4 = new String("Seat");
+
 		coche.add(c1);
 		coche.add(c2);
 		coche.add(c3);
 		coche.add(c4);
-		
-		
+
 		System.out.println(coche.size());
 		System.out.println();
 
-		//ejercicio3
-		
+		// ejercicio3
+
 		List<String> nombres = new ArrayList<>();
-		
+
 		nombres.add("Fran");
 		nombres.add("Iker");
 		nombres.add("Adriano");
 		nombres.add("Marina");
-		
-		for (int i = 0; i < nombres.size(); i ++) {
-			
-			System.out.println("Nombres: "+nombres.get(i));
-			
+
+		for (int i = 0; i < nombres.size(); i++) {
+
+			System.out.println("Nombres: " + nombres.get(i));
+
 		}
 		System.out.println();
 
-	String[] listaNom = {"Fran","Adriano","Iker","Marina"};
-	
-		for(String lista:listaNom) {
-			
+		String[] listaNom = { "Fran", "Adriano", "Iker", "Marina" };
+
+		for (String lista : listaNom) {
+
 			System.out.println(lista);
-			
-			
+
 		}
-		
+
 	}
 
 }
