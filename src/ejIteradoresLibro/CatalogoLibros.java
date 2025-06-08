2592b0aeb5ae3bf6e3b3c6fe797c0f08bdeaf2ab
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-08 15:36:50 +0200

    Agregar comentarios CatalogoLibros

--- src/ejIteradoresLibro/CatalogoLibros.java
+++ src/ejIteradoresLibro/CatalogoLibros.java
@@ -2,19 +2,17 @@ package ejIteradoresLibro;
 
 import java.util.*;
 
-public class CatalogoLibros implements Iterable<Libro>{
+public class CatalogoLibros implements Iterable<Libro> { // La clase implementa Iterable para permitir iteraciones con for-each
 
-	 ArrayList<Libro> libros = new ArrayList<Libro>();
-	
-	public void agregarLibro(Libro libro) {
-		
-		libros.add(libro);
-		
-	}
-	public Iterator<Libro> iterator(){
-		
-		return libros.iterator();
-		
-	}
-	
+    ArrayList<Libro> libros = new ArrayList<Libro>(); // Lista de libros almacenados en el catálogo
+
+    public void agregarLibro(Libro libro) { 
+        libros.add(libro); // Agrega un libro a la lista
+    }
+
+    public Iterator<Libro> iterator() { 
+        return libros.iterator(); // Devuelve un iterador para recorrer la lista de libros
+    }
 }
+	
+
